be8bfe72bd9215e08b4dbe6de8b3d52d08a6e7d6
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-22 19:49:32 +0800

    First Commit

--- src/Models/Student.php
+++ src/Models/Student.php
@@ -7,14 +7,32 @@ use \PDO;
 
 class Student extends BaseModel
 {
+    public $student_code, $email, $first_name, $last_name, $id;
 
     public function all()
     {
-        $sql = "SELECT * FROM students";
+        $sql = "SELECT id, student_code, CONCAT(first_name, ' ' , last_name) AS name FROM students";
         $statement = $this->db->prepare($sql);
         $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_CLASS, '\App\Models\Student');
-        return $result;
+
+        return $statement->fetchAll(PDO::FETCH_CLASS, '\App\Models\Student');
+    }
+
+    public function getStudentCode() {
+        return $this->student_code;
+    }
+
+    public function setStudentCode($code) {
+        $this->student_code = $code;
+    }
+
+    public function find($id) {
+        $sql = "SELECT * FROM students WHERE student_id = :student_id";
+        $statement = $this->db->prepare($sql);
+        $statement -> bindParam(':student_id', $id, PDO::PARAM_INT);
+        $statement -> execute();
+
+        return $statement -> fetchObject('App\Models\Student');
     }
 
 }
