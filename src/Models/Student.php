15fa96c28a38f7d9b317b972b2d9be15706f9823
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-25 22:42:52 +0800

    Added course_codes column

--- src/Models/Student.php
+++ src/Models/Student.php
@@ -11,7 +11,14 @@ class Student extends BaseModel
 
     public function all()
     {
-        $sql = "SELECT id, student_code, CONCAT(first_name, ' ', last_name) AS name, email, DATE_FORMAT(date_of_birth, '%M %d, %Y') AS birthdate, sex FROM students";
+        $sql = "SELECT s.student_code, CONCAT(first_name, ' ', last_name) AS name,
+                email, DATE_FORMAT(date_of_birth, '%M %d, %Y') AS birthdate, sex, GROUP_CONCAT(c.course_code ORDER BY c.course_code SEPARATOR ', ') AS course_codes
+                FROM students s LEFT JOIN course_enrollments ce
+                ON s.student_code = ce.student_code
+                LEFT JOIN courses c
+                ON ce.course_code = c.course_code
+                GROUP BY s.id";
+
         $statement = $this->db->prepare($sql);
         $statement->execute();
 
